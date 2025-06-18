1180e69d8c2600bef0dd8c050e516c342a275773
monka6 <daraartovskaa@gmail.com>
2025-06-18 13:43:27 +0300

    Initial commit for T3

---
+++ ovchinnikova.daria/T3/T3/Functions.h
@@ -0,0 +1,31 @@
+#ifndef FUNCTIONS_H_
+#define FUNCTIONS_H_
+#include <vector>
+#include <sstream>
+#include "Polygon.h"
+
+std::istream& operator>>(std::istream& in, Point& point);
+std::istream& operator>>(std::istream& in, Polygon& polygon);
+
+void invalidCommand();
+bool isEmpty(std::vector<Polygon>& data);
+
+double computeArea(std::vector<Point>& points);
+double areaEvenOdd(std::string& arg, std::vector<Polygon>& data);
+double areaMean(std::vector<Polygon>& data);
+double areaNumOfVertexes(int arg, std::vector<Polygon>& data);
+double maxAreaVertexes(std::string& arg, std::vector<Polygon>& data);
+double minAreaVertexes(std::string& arg, std::vector<Polygon>& data);
+int countEvenOdd(std::string arg, std::vector<Polygon>& data);
+int countVertexes(int arg, std::vector<Polygon>& data);
+bool isRightAngle(const Point& a, const Point& b, const Point& c);
+int countRects(std::vector<Polygon>& data);
+int orientation(const Point& a, const Point& b, const Point& point);
+bool pointOnSegment(const Point& a, const Point& b, const Point& point);
+bool intersectionOfSeg(const Point& a1, const Point& b1, const Point& a2, const Point& b2);
+bool pointInPolygon(const Point& point, const Polygon& polygon);
+bool polygonsIntersect(const Polygon& a, const Polygon& b);
+int countIntersectingPolygons(const std::vector<Polygon>& a, const Polygon& b);
+
+
+#endif
