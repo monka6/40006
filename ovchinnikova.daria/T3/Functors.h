26e1c1dd1fe87896bf53d485d800b4736f06d8f4
monka6 <daraartovskaa@gmail.com>
2025-06-18 14:06:55 +0300

    Initial commit for T3

---
+++ ovchinnikova.daria/T3/Functors.h
@@ -0,0 +1,18 @@
+#ifndef FUNCTORS_H_
+#define FUNCTORS_H_
+#include "Polygon.h"
+#include "Functions.h"
+
+struct VertexCountComparator {
+    bool operator()(const Polygon& a, const Polygon& b) const {
+        return a.points.size() < b.points.size();
+    }
+};
+
+struct AreaComparator {
+    bool operator()(Polygon& a, Polygon& b) const {
+        return computeArea(a.points) < computeArea(b.points);
+    }
+};
+
+#endif
